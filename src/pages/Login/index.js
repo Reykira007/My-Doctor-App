32cfc21127751bbf0ea1fab55d1363c29787b100
Reykira007 <rearryc@gmail.com>
2023-12-22 01:46:23 +0700

    Slicing login pages

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,12 +1,34 @@
-import {StyleSheet, Text, View} from 'react-native';
 import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+import {ILLogo} from '../../assets';
+import {Button, Gap, Input, Link} from '../../components/atoms';
 
 export default function Login() {
   return (
-    <View>
-      <Text>Login Page</Text>
+    <View style={styles.page}>
+      <ILLogo />
+      <Text style={styles.title}>Masuk dan mulai berkontribusi</Text>
+      <Input label="Email" />
+      <Gap height={24} />
+      <Input label="Password" />
+      <Gap height={10} />
+      <Link title="Forgot My Password" size={12} />
+      <Gap height={40} />
+      <Button title="Sign In" />
+      <Gap height={30} />
+      <Link title="Create New Account" size={16} align="center" />
     </View>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  page: {padding: 40, flex: 1, backgroundColor: 'white'},
+  title: {
+    fontSize: 20,
+    fontFamily: 'Nunito-SemiBold',
+    color: '#112340',
+    marginTop: 40,
+    marginBottom: 40,
+    maxWidth: 153,
+  },
+});
